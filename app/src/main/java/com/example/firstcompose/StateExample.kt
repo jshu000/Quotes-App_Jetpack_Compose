7f2aca64a154f370f444113b0fa177a340511754
Jshu <56064412+jshu000@users.noreply.github.com>
2024-09-27 20:18:28 +0530

    Before State Hoisting example
    Before State Hoisting example

--- app/src/main/java/com/example/firstcompose/StateExample.kt
+++ app/src/main/java/com/example/firstcompose/StateExample.kt
@@ -1,10 +1,17 @@
 package com.example.firstcompose
 
 import android.util.Log
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Favorite
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
@@ -14,6 +21,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Preview
 @Composable
@@ -23,6 +31,7 @@ fun NotificationScreen(){
         modifier = Modifier.fillMaxSize(1f)) {
 
         NotificationCounter();
+        MessageBar()
     }
 }
 
@@ -39,3 +48,23 @@ fun NotificationCounter() {
         }
     }
 }
+
+@Composable
+fun MessageBar() {
+    Card(
+        elevation = CardDefaults.cardElevation(4.dp)
+    ) {
+        Row(
+            Modifier.padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                imageVector = Icons.Outlined.Favorite,
+                contentDescription = "",
+                Modifier.padding(4.dp)
+            )
+            Text(text = "Messages sent so far - 10")
+        }
+    }
+}
+
