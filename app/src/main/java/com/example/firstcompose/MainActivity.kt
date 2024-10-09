86f38aa2600db8be1248e4735591f5cc5bae79e8
Jshu <56064412+jshu000@users.noreply.github.com>
2024-10-10 02:50:56 +0530

    Quote Detail Screen is Completed
    Jetpack Compose - Simple Quotes App + Layout Design & Demo

--- app/src/main/java/com/example/firstcompose/MainActivity.kt
+++ app/src/main/java/com/example/firstcompose/MainActivity.kt
@@ -20,7 +20,6 @@ import com.example.firstcompose.screens.QuoteListItem
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             QuoteListItem()
         }
--- app/src/main/java/com/example/firstcompose/screens/QuoteListItem.kt
+++ app/src/main/java/com/example/firstcompose/screens/QuoteListItem.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -22,6 +23,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.text.font.FontWeight
@@ -68,6 +70,48 @@ fun QuoteListItem() {
 
 @Preview
 @Composable
-private fun QuoteDetail() {
-    
+fun QuoteDetail() {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxSize(1f)
+            .background(
+                Brush.sweepGradient(
+                    colors = listOf(
+                        Color(0xFFFFFFFF),
+                        Color(0xFFE3E3E3)
+                    )
+                )
+            )
+    ) {
+        Card(
+            elevation = CardDefaults.cardElevation(defaultElevation = 4.dp) ,
+            modifier = Modifier.padding(30.dp)
+        ) {
+            Column(modifier = Modifier.padding(32.dp)) {
+                Image(
+                    imageVector = Icons.Filled.FormatQuote,
+                    colorFilter = ColorFilter.tint(Color.White),
+                    contentDescription = "Quote",
+                    alignment = Alignment.CenterStart,
+                    modifier = Modifier
+                        .size(80.dp)
+                        .rotate(180f)
+                        .background(Color.Black)
+                )
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = "Time is most valuable thingggggggggggggggggggg fvrger gertfg eg rtg ",
+                    //style = MaterialTheme.typography.h6,
+                    modifier = Modifier.padding(0.dp, 0.dp, 0.dp, 8.dp)
+                )
+                Text(
+                    text = "jashwanrfefg",
+                    //style = MaterialTheme.typography.body1,
+                    fontWeight = FontWeight.Thin,
+                    modifier = Modifier.padding(top = 4.dp)
+                )
+            }
+        }
+    }
 }
