a994261d07175495750378ba470a97d607eca68a
Jshu <56064412+jshu000@users.noreply.github.com>
2024-10-15 10:45:55 +0530

    Side Effect Handlers
    Jetpack Compose - RememberUpdatedState | Side Effect Handlers |
    Side Effect is produced as value in the log statement is not updated while the text UI is updating

---
+++ app/src/main/java/com/example/firstcompose/MainActivity2.kt
@@ -0,0 +1,53 @@
+package com.example.firstcompose
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import kotlinx.coroutines.delay
+
+class MainActivity2 : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            App2()
+        }
+    }
+}
+
+
+@Composable
+fun App2() {
+    var counter = remember { mutableStateOf(0) }
+
+    LaunchedEffect(Unit) {
+        delay(2000)
+        counter.value = 10
+    }
+
+    Counter(counter.value)
+}
+
+@Composable
+fun Counter(value: Int) {
+    LaunchedEffect(key1 = Unit) {
+        delay(5000)    // Assuming this is an heavy operation . So execution of this recommended for 1 time only.
+        Log.d("Jashwant", "Counter: -${value.toString()}")
+    }
+    Text(text = value.toString())
+}
