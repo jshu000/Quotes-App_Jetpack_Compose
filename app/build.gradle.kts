58a158f5dd772a61320911358fc8c2c23354fc00
Jashwant <jashwant999@gmail.com>
2026-02-16 11:13:45 +0530

    8.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.andha_coder.firstcompose"
         minSdk = 24
         targetSdk = 36
-        versionCode = 7
-        versionName = "7.0"
+        versionCode = 8
+        versionName = "8.0"
 
         vectorDrawables.useSupportLibrary = true
 
